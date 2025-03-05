1da1218447e7a89f0dbfff43e61591912ac7b556
stephy <sbcapuno@paterostechnologicalcollege.edu.ph>
2025-03-05 19:36:20 +0800

    Added Capuno.java and updated week5.java

--- capuno.java
+++ capuno.java
@@ -1,6 +1,6 @@
 public class Capuno {
     public static void display() {
-        int age = 21; // 
+        int age = 21; // Replace with your actual age
         System.out.println("Hello! This is Capuno contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
