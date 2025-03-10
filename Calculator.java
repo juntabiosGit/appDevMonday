5643d10610e75130956d0ec408adca86b97e0ecc
FryncesRomero <faromero@paterostechnologicalcollege.edu.ph>
2025-03-10 21:12:59 +0800

    Update Calculator.java
    adjusting the members name on comment

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class GroupWork {
     public static int max(int a, int b) {
+
+        /*Group 8 Members: Carlito A. Sese Jr.
+                        Ma. Eloisa Louisse Corporal
+                        Christian Dolloso
+                        Frynces Avryl I. Romero
+                        Robel Padohinog
+                        Bryle Lacandula*/
+        
       if (a>b) {
         System.out.println("Max number is: "+ a); }
        else if (b>a) {
