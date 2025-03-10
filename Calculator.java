44e0a23705b426d5e49b40caa43f156210ecb1f4
FryncesRomero <faromero@paterostechnologicalcollege.edu.ph>
2025-03-10 20:54:22 +0800

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class GroupWork {
+    public static int max(int a, int b) {
+      if (a>b) {
+        System.out.println("Max number is: "+ a); }
+       else if (b>a) {
+         System.out.println("Max number is: "+ b); 
+       }
+      return 0;       
+    };
+  }
