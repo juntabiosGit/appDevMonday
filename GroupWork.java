786740e04eee4077a55c6c8198751fba902b9d0f
marcoredecio <jmredecio@paterostechnologicalcollege.edu.ph>
2025-03-10 21:21:39 +0800

    GroupWork.java

--- GroupWork.java
+++ GroupWork.java
@@ -9,6 +9,7 @@ public class GroupWork {
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
+        
         // Calling methods from Calculator.java
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
         System.out.println("Difference: " + Calculator.subtract(num1, num2));  // Group 2's method
