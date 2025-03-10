0f66d2eab7182ae0c1925f2b5fef2c02014ce975
LouieLucero <lucerolouiejustine@gmail.com>
2025-03-10 20:37:04 +0800

    Added Group2 Substraction Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,19 @@
 // Calculator.java
 public class Calculator {
-    
-    // Group 1: Addition (Members: John, Sarah, Alex)
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
-    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
-    public static boolean isEqual(int a, int b) {
-        return a == b;
+    /* Group 2: Addition (Members: 
+        San Agustin, Aaron
+        Aldama, Allane
+        Palomas, Ralph Allen M.
+        Arenas, Emely
+        Jalosjos, Emmanuel
+        Marasigan, Gilbert Jr
+        David, Whynzel Lance
+        Lucero, Louie Justine
+        Mendoza, Reign
+        Plena, Vincent Allen)
+    */
+    public static int subtract(int a, int b) {
+        return a - b;
     }
-
-    // Other groups will add their methods here
 }
