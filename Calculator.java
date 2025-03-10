96d60a117e4a7ab7fa273437ef91e3f40a3bc09d
LouieLucero <lucerolouiejustine@gmail.com>
2025-03-10 20:44:04 +0800

    Added Group2 Method Subtraction (Members: Lucero, Louie Justine)

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,19 @@
-// Calculator.java
-public class Calculator {
 
-    /* Group 2: Addition (Members: 
-        San Agustin, Aaron
-        Aldama, Allane
-        Palomas, Ralph Allen M.
-        Arenas, Emely
-        Jalosjos, Emmanuel
-        Marasigan, Gilbert Jr
-        David, Whynzel Lance
-        Lucero, Louie Justine
-        Mendoza, Reign
-        Plena, Vincent Allen)
-    */
+public class Calculator {
+// Group 1: Addition (Members: John, Sarah, Alex)
+public static int add(int a, int b) {
+    return a + b;
+}
+
+
+    // Group 2: Subtraction  (Members: Lucero,Louie Justine)
     public static int subtract(int a, int b) {
         return a - b;
+    }
+
+
+    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
+    public static boolean isEqual(int a, int b) {
+        return a == b;
     }
 }
--- GroupWork.java
+++ GroupWork.java
@@ -9,7 +9,9 @@ public class GroupWork{
         int num2 = scanner.nextInt();
 
         // Calling methods from Calculator.java
+        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
         System.out.println("Difference: " + Calculator.subtract(num1, num2));  // Group 2's method
+        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
         scanner.close();
     }
 }
