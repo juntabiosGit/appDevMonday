f905e9034cb5c8ad87e8cfe1143e05e3506a21a7
NateWalo <natewalo@gmail.com>
2025-03-10 20:37:57 +0800

    Added square method

--- Calculator.java
+++ Calculator.java
@@ -5,14 +5,15 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
-    // Group 6: Square (Members: Acosta, Bantilo, Jalique, Pergis, Walo)
-    public static int square(int a) {
-        return a * a;
-    }
+    
     // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
     public static boolean isEqual(int a, int b) {
         return a == b;
     }
 
     // Other groups will add their methods here
+    // Group 6: Square (Members: Acosta, Bantilo, Jalique, Pergis, Walo)
+    public static int square(int a) {
+        return a * a;
+    }
 }
