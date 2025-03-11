6b8494a420117bb0b2cecdc9814ad3d717777092
NikkaCatalan <ncatalan@paterostechnologicalcollege.edu.ph>
2025-03-11 11:11:11 +0800

    Group 1: (Members: Catalan)

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 
 public class Calculator {
-// Group 1: Addition (Members: John, Sarah, Alex)
+// Group 1: Addition (Members: Catalan, Sarah, Alex)
 public static int add(int a, int b) {
     return a + b;
 }
