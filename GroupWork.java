cebf8c6572a49ec9c1c21bbcc93d8ffca393e810
Vincent Allen Plena <vaplena@paterostechnologicalcollege.edu.ph>
2025-03-17 17:55:16 +0800

    Updated and running files

--- GroupWork.java
+++ GroupWork.java
@@ -2,68 +2,23 @@ import java.util.Scanner;
 
 public class GroupWork {
 
-
-
-public class GroupWork {
-
-public class test {
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
-
-
         System.out.println("Difference: " + Calculator.subtract(num1, num2));  // Group 2's method
-        scanner.close();
-    }
-}
-
-
         System.out.println("Sum: " + Calculator.add(num1, num2)); // Group 1's method
-
         System.out.println("Product " + Calculator.multiply(num1, num2)); // Group 3's method
-
         System.out.println("Squared " + Calculator.square(num1)); // Group 6's method
-
-        System.out.println("Difference: " + Calculator.subtract(num1, num2)); // Group 2's method
-
         System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2)); // Group 10's method
+        //System.out.println(Calculator.max(num1, num2));  // Group 8's method
+	    //System.out.println("Cube: " + Calculator.cube(num1));
+		System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4's method
+        //System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
+        //System.out.println("Smaller Number is: " + Calculator.isSmall(num1, num2)); //Group 9's method
         scanner.close();
     }
 }
-
-
-        System.out.println(Calculator.max(num1, num2));  // Group 8's method
-
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-
-        System.out.print	
-	System.out.println("Cube: " + Calculator.cube(num1));
-
-		
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4's method
-
-
-        System.out.println("Difference: " + Calculator.subtract(num1, num2));  // Group 2's method
-	
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-
-
-        System.out.println("Smaller Number is: " + Calculator.isSmall(num1, num2)); //Group 9's method
-
-        scanner.close();
-    }
-
 
