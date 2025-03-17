4c8402180ad914206cc31f0097da68026f4a1650
ludavid@paterostechnologicalcollege.edu.ph <ludavid@paterostechnologicalcollege.edu.ph>
2025-03-17 16:10:46 +0800

    Added Subtraction Method (Members: David, Lance Whynzel)

--- GroupWork.java
+++ GroupWork.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class GroupWork{
+public class GroupWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -9,14 +9,7 @@ public class GroupWork{
         int num2 = scanner.nextInt();
 
         // Calling methods from Calculator.java
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-
-        System.out.println("Squared " + Calculator.square(num1)); //Group 6's method
-
         System.out.println("Difference: " + Calculator.subtract(num1, num2));  // Group 2's method
-
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
         scanner.close();
     }
 }
-
