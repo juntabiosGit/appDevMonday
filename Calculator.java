ebd086c588f53f39042e4fc8bd5f0857a589df1b
MirandillaJoshua10 <jhmirandilla@paterostechnologicalcollege.edu.ph>
2025-03-17 11:13:35 +0800

    Group 3: Added multiplication method ( Members: Baeta, Dela Costa, Demafelis, Larong, Mirandilla, Ponce, Rivera, Senires)

--- Calculator.java
+++ Calculator.java
@@ -1,30 +1,33 @@
 
 public class Calculator {
-// Group 1: Addition (Members: John, Sarah, Alex)
-public static int add(int a, int b) {
-    return a + b;
-}
-    // Group 2: Subtraction  (Members: Lucero, Louie Justine, San Agustin, Aaron, Aldama, Allane, Palomas, Ralph Allen M., Arenas, Emely, Jalosjos, Emmanuel, Marasigan, Gilbert Jr, David, Whynzel Lance, Mendoza, Reign, Plena, Vincent Allen))
+    // Group 1: Addition (Members: John, Sarah, Alex)
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Group 2: Subtraction (Members: Lucero, Louie Justine, San Agustin, Aaron,
+    // Aldama, Allane, Palomas, Ralph Allen M., Arenas, Emely, Jalosjos, Emmanuel,
+    // Marasigan, Gilbert Jr, David, Whynzel Lance, Mendoza, Reign, Plena, Vincent
+    // Allen))
     public static int subtract(int a, int b) {
         return a - b;
     }
 
-
-
     // Group 2: Subtraction (Members: John, Sarah, Alex)
     public static int subtract(int a, int b) {
-    return a - b;
+        return a - b;
     }
 
-
-
-
+    // Group 3: Multiplication (Members: Baeta, Dela Costa, Demafelis, Larong,
+    // Mirandilla, Ponce, Rivera, Senires)
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 
     // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
     public static boolean isEqual(int a, int b) {
         return a == b;
     }
-
 
     // Group 6: Square (Members: Acosta, Bantilo, Jalique, Pergis, Walo)
     public static int square(int a) {
@@ -40,4 +43,3 @@ public static int add(int a, int b) {
 }
 
 }
-
